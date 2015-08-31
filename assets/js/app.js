24464a348f02561b25faffe7e61ba92114f21c75
P.E. Butler III <p.e.butler3@gmail.com>
2015-08-31 12:38:21 -0600

    updated just about everything // slider.js breaks nav

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@
   var body = document.body;
   var burgerMenu = document.getElementsByClassName('b-menu')[0];
   var burgerContain = document.getElementsByClassName('b-container')[0];
-  var burgerNav = document.getElementsByTagName('nav')[0];
+  var burgerNav = document.getElementsByClassName('b-nav')[0];
 
   burgerMenu.addEventListener('click', function toggleClasses() {
     [body, burgerContain, burgerNav].forEach(function (el) {
